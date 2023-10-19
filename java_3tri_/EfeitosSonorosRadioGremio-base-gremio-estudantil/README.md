794088fcff44d4a89f7674a3c7e9d5e98603c12c
ProfMariafabieli02 <127252545+ProfMariafabieli02@users.noreply.github.com>
2023-10-19 15:21:42 -0300

    Delete java_3tri_/EfeitosSonorosRadioGremio-base-gremio-estudantil directory
    errro

--- java_3tri_/EfeitosSonorosRadioGremio-base-gremio-estudantil/README.md
+++
@@ -1,2 +0,0 @@
-# EfeitosSonorosRadioGremio
-Mesa de mídias com áudios para efeitos sonoros especiais da rádio Grêmio Estudantil
--- java_3tri_/EfeitosSonorosRadioGremio-base-gremio-estudantil/main.js
+++
@@ -1,5 +0,0 @@
-function tocaSomRisada(){
-    document.querySelector("#som_tecla_risada").play();
-}
-const listadeteclas=document.querySelectorAll(.'tecla');
-listadeteclas[0].onclick=tomSomaplausos;
