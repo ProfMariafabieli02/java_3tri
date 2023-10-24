4742c96d47b0e1ce0bb48d1538fb0398a82d73bc
ProfMariafabieli02 <127252545+ProfMariafabieli02@users.noreply.github.com>
2023-10-24 18:44:28 -0300

    01

--- main.js
+++ main.js
@@ -2,4 +2,10 @@ function tocaSomaplausos(){
     document.querySelector("#som_tecla_aplausos").play();
 }
 const listadeteclas=document.querySelectorAll('.tecla');
-listadeteclas[0].onclick=tomSomAplausos;
+let contador=0;
+while(contador <8) {
+
+listadeteclas[contador].onclick=tomSomAplausos;
+contador=contador +1 ;
+console.log(contador);
+}
