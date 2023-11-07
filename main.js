ccfe59cdd68ba3e34ddfceda78de14ffab6f0d95
ProfMariafabieli02 <127252545+ProfMariafabieli02@users.noreply.github.com>
2023-11-07 19:08:59 -0300

    for

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ function tocaSom(idElementoAudio){
     document.querySelector(idElementoAudio).play();
 }
 const listadeteclas=document.querySelectorAll('.tecla');
-let contador=0;
-while(contador <listadeteclas.length) {
+
+for(let contador=0;contador <listadeteclas.length; contador++) {
     const tecla=listadeteclas[contador];
-const efeito= teclas[contador].classList[1];
-const idAudio= `#som_${efeito} `;
-tecla.onclick=function(){
+    const efeito= teclas[contador].classList[1];
+    const idAudio= `#som_${efeito} `;
+    tecla.onclick=function(){
     tocaSom(idAudio);
 }
 contador=contador +1 ;
