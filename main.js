c1a0e68c1e9be320da85c2947c2bb51d984ab520
ProfMariafabieli02 <127252545+ProfMariafabieli02@users.noreply.github.com>
2023-11-14 19:08:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -15,4 +15,8 @@ for(let contador = 0; contador < listadeTeclas.length; contador++){
     tecla.onkeydown = function () {
         tecla.classList.add('ativa');
     }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa')
+    }
+        
 }
