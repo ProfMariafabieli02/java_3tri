3ca3ceed9671efd4af82f65b3e5029c45ad0e0ba
ProfMariafabieli02 <127252545+ProfMariafabieli02@users.noreply.github.com>
2023-11-14 18:49:17 -0300

    01

--- main.js
+++ main.js
@@ -7,14 +7,9 @@ for(let contador=0;contador <listadeteclas.length; contador++) {
     const tecla=listadeteclas[contador];
     const efeito= teclas[contador].classList[1];
     const idAudio= `#som_${efeito} `;
-    tecla.onclick=function(){
+    tecla.onclick= function(){
     tocaSom(idAudio);
 }
-
-//console.log(contador);
-
-
-
 tecla.onkeydown= function(){
-    tecla.add('ativa');
+    tecla.classList.add('ativa');
 }
